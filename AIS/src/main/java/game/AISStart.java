de1f7a42941500c461501e4952f674b4bc510808
sazannik <sazannik@fel.cvut.cz>
2023-05-12 05:58:16 +0200

    Small changes.

--- AIS/src/main/java/game/AISStart.java
+++ AIS/src/main/java/game/AISStart.java
@@ -4,19 +4,20 @@ import engine.panels.GamePanel;
 import engine.utils.Logger;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class AISStart {
     public static void main(String[] args) {
-        Logger logger = new Logger("GameLogger.log");
+        Logger logger = new Logger("GamePanelLogger.log");
         logger.startLogging();
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setResizable(false);
+        window.setPreferredSize(new Dimension(1920, 1080));
         window.setTitle("Adapt. Improve. Survive.");
         window.setExtendedState(JFrame.MAXIMIZED_BOTH);
         window.setUndecorated(true);
         logger.info(window, "Window options: {defaultCloseOperation = JFrame.EXIT_ON_CLOSE, " +
-                "resizable = false, title = \"Adapt. Improve. Survive.\", extendedState = JFrame.MAXIMIZED_BOTH, " +
+                "resizable = true, title = \"Adapt. Improve. Survive.\", extendedState = JFrame.MAXIMIZED_BOTH, " +
                 "undecorated = true}.");
 
         GamePanel gamePanel = new GamePanel(1920, 1080);
